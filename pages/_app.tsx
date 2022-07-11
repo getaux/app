1d1aa789865852b75c0350e193a8e708be796c14
sam <sam.barrowclough@gmail.com>
2022-07-11 12:40:33 +0100

    https://linear.app/snoopy/issue/SNO-1460/loader-on-front-screen

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,9 +2,12 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { ImxContextProvider } from 'utils/useImx'
 import { NextUIProvider } from '@nextui-org/react'
-
+import { useEffect, useState } from 'react'
 import { configureChains, chain, createClient, WagmiConfig } from 'wagmi'
 import { publicProvider } from 'wagmi/providers/public'
+import { useRouter } from 'next/router'
+import { Logo } from 'components/icons'
+import Router from 'next/router';
 
 const { chains, provider, webSocketProvider } = configureChains(
   [chain.mainnet],
@@ -18,6 +21,35 @@ const client = createClient({
 })
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter()
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    Router.events.on('routeChangeStart', () => setLoading(true));
+    Router.events.on('routeChangeComplete', () => setLoading(false));
+    Router.events.on('routeChangeError', () => setLoading(false));
+    return () => {
+      Router.events.off('routeChangeStart', () => setLoading(true));
+      Router.events.off('routeChangeComplete', () => setLoading(false));
+      Router.events.off('routeChangeError', () => setLoading(false));
+    };
+  }, [Router.events]);
+
+  if (loading) {
+    return (
+      <div
+        style={{
+          zIndex: '100',
+          position: 'fixed',
+          top: '50%',
+          left: '50%',
+          transform: 'translate(-50%, -50%)',
+        }}
+      >
+        <Logo/>
+      </div>
+    )
+  }
+
   return (
     <WagmiConfig client={client}>
       <NextUIProvider>
